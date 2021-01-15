682f96920d822a4f2d6f00d7d033e43989509b6d
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-01-15 00:45:15 -0400

    first component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;
+import reactDOM from 'react-dom';
+import Badge from './components/badge';
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+reactDOM.render(<Badge/>, container);
+
+
+
+
+
+
