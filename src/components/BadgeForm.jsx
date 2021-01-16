8266516846262f83df5d4d35d3434fe700cae9f0
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-01-16 06:09:48 -0400

    Badge Form.jsx

--- src/components/BadgeForm.jsx
+++ src/components/BadgeForm.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+  handleChange = (e) => {
+    console.log({ 
+      name: e.target.name,
+      value: e.target.value 
+    });
+  };
+
+  handleClick = (e) => {
+    console.log(
+      "Button actived"
+    )
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('Form was delivered');
+  }
+
   render(){
     return (
       <section>
         <h1>New Attend</h1>
-          <form >
+          <form onSubmit={this.handleSubmit} >
             <div className="form-group">
               <label>First name</label>
-              <input className="form-control" type="text" name="first-name"/>
+              <input onChange={this.handleChange} className="form-control" type="text" name="first-name"/>
             </div>
-            <button className="btn btn-primary">Send it!</button>
+            <button  onClick={this.handleClick} className="btn btn-primary">Send it!</button>
           </form>
       </section>
     )
